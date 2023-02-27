e069d9090f8621081d95510d9cd087a7dc3e0399
Alex <freedom20011987@gmail.com>
2023-02-27 22:48:47 +0700

    hw_2

--- Conf.py
+++
@@ -1,8 +0,0 @@
-DB_ARGS = {
-    'db': 'home_credit',
-    'user': os.getenv('DB_USER'),
-    'host': '127.0.0.1',
-    'password': os.getenv('DB_PASSWORD')
-}
-
-DATA_FULL_PATH = "C:\\Program Files\\PostgreSQL\\14\\data\\"
